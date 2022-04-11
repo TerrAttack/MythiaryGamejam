e2dc5de75ae908272a05ff31edf503937b61aa76
EpicBackflip <nyla.slot@hva.nl>
2022-04-11 11:08:26 +0200

    setting up some base stuff

--- Source/MythiaryGamejam/Private/PEnemy.cpp
+++ Source/MythiaryGamejam/Private/PEnemy.cpp
@@ -11,6 +11,17 @@ void APEnemy::BeginPlay()
 	Super::BeginPlay();
 }
 
+void APEnemy::Move()
+{
+	
+}
+
+void APEnemy::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+                             int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
 void APEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- Source/MythiaryGamejam/Public/PEnemy.h
+++ Source/MythiaryGamejam/Public/PEnemy.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
 #include "PEnemy.generated.h"
 
@@ -12,10 +13,15 @@ class MYTHIARYGAMEJAM_API APEnemy : public AActor
 	
 public:	
 	APEnemy();
-
 protected:
 	virtual void BeginPlay() override;
-
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 public:	
 	virtual void Tick(float DeltaTime) override;
+	void Move();
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* BaseMesh;
+	UPROPERTY(EditAnywhere)
+	USphereComponent* HitBox;
 };
